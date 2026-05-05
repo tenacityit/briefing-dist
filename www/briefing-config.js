81a0f877ce0045c141fe00a624007cc41492e1be
tenacityit <133159646+tenacityit@users.noreply.github.com>
2026-05-05 11:16:46 -0400

    Configure TURN server credentials for WebRTC reliability

--- www/briefing-config.js
+++ www/briefing-config.js
@@ -1,3 +1,13 @@
 // Don't remove this file!
 
 // Visit https://github.com/holtwick/briefing/ for more details.
+
+window.briefingConfig = {
+  ICE_SERVERS: [
+    {
+      urls: ["turn:turn.tenacity.it:3478", "turns:turn.tenacity.it:5349"],
+      username: "briefing",
+      credential: "05ad164f898435377e9b00605ab48c4b072ba6acee37f02e76b69ce2baac3182"
+    }
+  ]
+}
